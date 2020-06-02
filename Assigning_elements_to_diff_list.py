8d16626a91f41300ac17be777b962f22a97d7b1b
tushshah10 <66304959+tushshah10@users.noreply.github.com>
2020-06-02 17:43:32 +0530

    Create Assigning_elements_to_diff_list.py

---
+++ Assigning_elements_to_diff_list.py
@@ -0,0 +1,9 @@
+list1=[]
+list2=[]
+a=int(input("Enter element to be assigned to first position"))
+list1.append(a);
+a=int(input("Enter element to be assigned to second position"))
+list1.append(a);
+x=list1[0];
+list2.append(x);
+print("Second list is",list2)
